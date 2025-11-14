fbfa291eca6fb3681c5a2b720534627707569e16
Oleksii Mykhailenko <8993077@gmail.com>
2025-11-14 12:29:50 +0200

    first commit - added files

---
+++ README.md
@@ -0,0 +1,4 @@
+                Commands:
+           1. g++ -c calculator_fn.cpp;
+           2. g++ -c main.cpp;
+           3. g++ calculator_fn.cpp main.cpp -o my_program.out
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "calculator.h"
+
+int main(){
+    int sum_result = add(1, 10);
+
+    std::cout << "The result is - " << sum_result << std::endl;
+    return 0;
+}
